a6d87cc123c1fd2d7ac82cfb7ffa03bade369aed
swaleh0757224035-ui <swaleh0757224035@gmail.com>
2025-08-26 19:13:03 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -2,7 +2,7 @@
 module.exports = {
   SESSION_ID: "",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "254112263776", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
