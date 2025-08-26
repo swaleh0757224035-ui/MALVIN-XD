221dc6fc3d22430b8b14073c2ccf4c079bffdfa0
swaleh0757224035-ui <swaleh0757224035@gmail.com>
2025-08-26 18:32:03 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,34 @@
 
 module.exports = {
+  SESSION_ID: "",malvin~Q2dG3Sra#ruI2CDy5iwTAfr8CbCw7AcxWM9zXhchS51BWn6lz0tc  // add your Session Id here
+  
+  OWNER_NUMBER: "254112263776", // put your phone number here
+  
+  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  
+  TIMEZONE: "Africa/Harare" //put your country timeZone6:29....leave blank if u don't know.
+};
+module.exports = {
+  SESSION_ID: "",malvin~Q2dG3Sra#ruI2CDy5iwTAfr8CbCw7AcxWM9zXhchS51BWn6lz0tc  // add your Session Id here
+  
+  OWNER_NUMBER: "254112263776, // put your phone number here
+  
+  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  
+  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+};
+module.exports = {
   SESSION_ID: "",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "254112263776", // put your phone number here
+  
+  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  
+  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+};module.exports = {
+  SESSION_ID: "",  // add your Session Id here
+  
+  OWNER_NUMBER: "254112263776", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
